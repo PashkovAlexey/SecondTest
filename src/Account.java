e1278f7ef085fcc4281b3d133c3d79624f8c86a2
pashalan <pashalan@mail.ru>
2022-01-13 21:50:04 +0300

    This is the first commit of this test project

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private double money;
+
+    public Account (double money){
+        this.money = money;
+    }
+
+
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+
+
+}
---
+++ src/Bank.java
@@ -0,0 +1,7 @@
+public class Bank {
+    public static void main(String[] args) {
+        Account myAccount = new Account(100500);
+
+
+    }
+}
