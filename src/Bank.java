32323187620315fcda40488fb2ca60b7462f7f6a
pashalan <pashalan@mail.ru>
2022-01-13 21:53:44 +0300

    I changed the amount of money on the account

--- src/Bank.java
+++ src/Bank.java
@@ -2,6 +2,8 @@ public class Bank {
     public static void main(String[] args) {
         Account myAccount = new Account(100500);
         System.out.println("Денег на счету " + myAccount.getMoney() + " рублей");
+        myAccount.setMoney(234);
+        System.out.println("Теперь денег " + myAccount.getMoney() + " рублей");
 
 
     }
