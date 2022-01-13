225a6688e1d3e9002cf98617d343832af01db7d3
pashalan <pashalan@mail.ru>
2022-01-13 21:51:50 +0300

    I added the function to print how much money are there

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,7 @@
 public class Bank {
     public static void main(String[] args) {
         Account myAccount = new Account(100500);
+        System.out.println("Денег на счету " + myAccount.getMoney() + " рублей");
 
 
     }
