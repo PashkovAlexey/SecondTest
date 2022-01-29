70571f36360b8daf2548a50602a34da3ac50750a
Evgeny Skorokhodov <95185633+LosAng2022@users.noreply.github.com>
2022-01-29 11:36:51 +0300

    обновил

--- src/Bank.java
+++ src/Bank.java
@@ -7,6 +7,12 @@ public class Bank {
         myAccount.setMoney(234);
         System.out.println("Теперь денег " + myAccount.getMoney() + " рублей");
 
+        Bandit madDog = new Bandit();
+        myAccount.setMoney(madDog.StealMoney(myAccount.getMoney()));
+        System.out.println("Бешеный Пёс украл денег " + madDog.getStealMoney());
+        System.out.println("Теперь в банке денег " + myAccount.getMoney());
+
+
         Hacker hacker = new Hacker();
         hacker.stealMoney(myAccount);
 
